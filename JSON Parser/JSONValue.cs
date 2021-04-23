e9c109442fa720bb1a12f68b20f15c86983a9215
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-04-23 15:46:47 +0300

    Add parser design

---
+++ JSON Parser/JSONValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSON_Parser
+{
+    abstract class JSONValue
+    {
+
+    }
+
+    abstract class SingleJSONValue<T> : JSONValue
+    {
+        public T Value { get; set; }
+    }
+
+    abstract class CollectionJSONValue<T> : JSONValue
+    {
+        public List<T> Value { get; set; }
+    }
+
+    class StringJSONValue : SingleJSONValue<string> { }
+    class NumberJSONValue : SingleJSONValue<double> { }
+    class BoolenJSONValue : SingleJSONValue<bool> { }
+    class NullJSONValue : SingleJSONValue<bool?> { }
+
+    class ObjectJSONValue : CollectionJSONValue<KeyValue> { }
+    class ArrayJSONValue : CollectionJSONValue<JSONValue> { }
+
+    class KeyValue {
+        public string key;
+        public JSONValue value;
+    }
+
+    class JSON
+    {
+        public static JSONValue parse(string str) { return null; }
+    }
+}
